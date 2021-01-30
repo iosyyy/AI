dfe22ff956521fdae6ed8d4d5e9e8a4e9d834b3c
Aurora-NB <772335766@qq.com>
2021-01-30 13:50:09 +0800

    增加了基本样式

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import './App.css';
 import React from "react";
-import {Exhibition} from "./comps/Exhibition";
-import {api} from './config/api'
+import { Layout, Menu, Breadcrumb } from "antd";
+
+import "./App.css";
+import "antd/dist/antd.css";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div>
-      Hello World
-        <Exhibition source={api['ip']}/>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">普通訓練</Menu.Item>
+            <Menu.Item key="2">聯邦訓練</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">Content</div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          AI Demo ©2021 Created by Hrbust
+        </Footer>
+      </Layout>
+      ,
     </div>
   );
 }
