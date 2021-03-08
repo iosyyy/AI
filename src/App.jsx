a801f113da6f15c737a32cf9f2883824cafc94bc
name <626797813@qq.com>
2021-03-08 14:32:42 +0800

    #init

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,15 @@ import TrainingDetails from './pages/Training/Detail'
 import PubSubJS from "pubsub-js";
 import FederalDetailAll from "./pages/FederalDetail";
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content, Footer} = Layout;
+
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state={page:"1"}
+        this.state = {page: "1"}
         PubSubJS.subscribe('isRunning', (msg, data) => {
-          console.log(data)
-          this.setState({page: data.page})
+            console.log(data)
+            this.setState({page: data.page})
         })
     }
 
@@ -29,10 +30,22 @@ class App extends Component {
             <div>
                 <Layout className="layout">
                     <Header>
-                        <div className="logo"/>
-                        <Menu theme="dark" mode="horizontal" selectedKeys={[this.state.page]} onSelect={(info)=>{
+                        <div style={{
+                            width: '180px',
+                            lineHeight: "30px"
+
+                        }} className="logo">
+                            <span style={{
+                                WebkitBackgroundClip: "text",
+                                color: 'transparent',
+                                textAlign: "center",
+                                backgroundImage: "-webkit-linear-gradient(45deg, rgb(106,65,195), rgb(225,143,143)" +
+                                    ", rgb(224,43,122), rgb(150,55,187))",
+                            }}>数据价值多方安全共享平台</span>
+                        </div>
+                        <Menu theme="dark" mode="horizontal" selectedKeys={[this.state.page]} onSelect={(info) => {
                             this.setState({
-                              page:info.key
+                                page: info.key
                             })
                         }}>
                             <Menu.Item key="1">
@@ -67,15 +80,15 @@ class App extends Component {
                         </Switch>
                     </Content>
                     <div
-        style={{
-          width: "100%",
-          textAlign: "center",
-          position: "fixed",
-          bottom: 0,
-        }}
-      >
-        <p>AI Demo ©2021 Created by Hrbust Science and Technology University</p>
-      </div>
+                        style={{
+                            width: "100%",
+                            textAlign: "center",
+                            position: "fixed",
+                            bottom: 0,
+                        }}
+                    >
+                        <p>AI Demo ©2021 Created by Hrbust Science and Technology University</p>
+                    </div>
                 </Layout>
 
             </div>
