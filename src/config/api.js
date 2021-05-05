b6eda26b9bc7f1ce75da92963dd5dfa5e4ef6467
iosyyy <626797813@qq.com>
2021-05-05 19:15:16 +0800

    #fix 完成history页面

--- src/config/api.js
+++ src/config/api.js
@@ -6,7 +6,8 @@ const e = {
   normalTrain: host + api1 + "/base/train/",
   federalTrain: host + api1 + "/fed/service/",
   host,
-  serverSocketIp: 'http://localhost:8080',
+  serverSocketIp: "http://localhost:8080",
+  pageList: "http://127.0.0.1:8080/job/query/page/new",
 };
 
-export default e
+export default e;
--- src/pages/TrainingRecord/index.jsx
+++ src/pages/TrainingRecord/index.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { Button, Image, Input, Space, Table } from "antd";
+import { Button, Form, Image, Input, Select, Space, Table } from "antd";
 import NoteImg from "../../img/Note.png";
 import NoteHover from "../../img/NoteHover.png";
 import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 import axios from "axios";
+import api from "../../config/api";
+
+const { Option } = Select;
 
 class TrainingRecord extends Component {
   constructor(props) {
@@ -45,6 +48,10 @@ class TrainingRecord extends Component {
         title: <div>开始时间</div>,
         dataIndex: "startTime",
         key: "startTime",
+        sorter: {
+          compare: (a, b) => a.startTime - b.startTime,
+          multiple: 1,
+        },
         render: (text) => {
           return <>{new Date(text).toLocaleString()}</>;
         },
@@ -53,6 +60,10 @@ class TrainingRecord extends Component {
         title: <div>结束时间</div>,
         dataIndex: "endTime",
         key: "endTime",
+        sorter: {
+          compare: (a, b) => a.endTime - b.endTime,
+          multiple: 2,
+        },
         render: (text) => {
           return <>{new Date(text).toLocaleString()}</>;
         },
@@ -61,12 +72,15 @@ class TrainingRecord extends Component {
         title: <div>运行时间</div>,
         dataIndex: "duration",
         key: "duration",
+        sorter: {
+          compare: (a, b) => a.duration - b.duration,
+          multiple: 3,
+        },
         render: (text, value) => {
           let time = value.endTime - value.startTime;
           let seconds = Math.floor((time / 1000) % 60);
           let minutes = Math.floor((time / 1000 / 60) % 60);
           let hour = Math.floor((time / 1000 / 60 / 60) % 60);
-          console.log(seconds);
           return (
             <>
               {(hour < 10 ? "0" + hour : hour) +
@@ -163,13 +177,14 @@ class TrainingRecord extends Component {
       selectKey: "",
       NoteNow,
       loading: true,
+      pageSize: 0,
     };
   }
 
   componentDidMount() {
     // TODO 增加ajax
     axios
-      .post("http://127.0.0.1:8080/job/query/page/new", {
+      .post(api.pageList, {
         fDescription: "",
         jobId: "",
         job_id: "",
@@ -184,28 +199,42 @@ class TrainingRecord extends Component {
         status: [],
       })
       .then((r) => {
+        console.log(r);
         let list = r.data.data.list;
-        let dataSource = [];
-        list.forEach((values, key) => {
-          let value = values["job"];
-          dataSource.push({
-            key: key,
-            id: value.fJobId,
-            startTime: value.fStartTime,
-            endTime: value.fEndTime,
-            duration: value.fElapsed,
-            role: value.fRole,
-            partyId: value.fPartyId,
-            notes: value.fDescription,
-            status: value.fStatus,
-            action: value.fStatus === "success" ? "retry" : "",
-          });
-        });
+        let pageSize = r.data.data.totalRecord;
+        let dataSource = this.getDataSourceByDataList(list);
         this.setState({
           dataSource,
           loading: false,
+          pageSize,
+        });
+      })
+      .catch(() => {
+        // TODO 把异常显示到页面中
+        this.setState({
+          loading: false,
         });
       });
+  }
+
+  getDataSourceByDataList(list) {
+    let dataSource = [];
+    list.forEach((values, key) => {
+      let value = values["job"];
+      dataSource.push({
+        key: key,
+        id: value.fJobId,
+        startTime: value.fStartTime,
+        endTime: value.fEndTime,
+        duration: value.fElapsed,
+        role: value.fRole,
+        partyId: value.fPartyId,
+        notes: value.fDescription,
+        status: value.fStatus,
+        action: value.fStatus === "success" ? "" : "retry",
+      });
+    });
+    return dataSource;
   }
 
   setNoteShow(states, value) {
@@ -228,13 +257,174 @@ class TrainingRecord extends Component {
   render() {
     return (
       <div className="site-layout-content">
+        <div style={{ float: "right" }}>
+          <Form
+            size={"small"}
+            layout="inline"
+            onFinish={(res) => {
+              this.setState({
+                loading: true,
+              });
+              axios
+                .post(api.pageList, {
+                  fDescription: "",
+                  jobId: res.id,
+                  job_id: res.id,
+                  note: res.note,
+                  orderField: "f_job_id",
+                  orderRule: "desc",
+                  pageNum: 1,
+                  pageSize: 20,
+                  partyId: res.partyId,
+                  party_id: res.partyId,
+                  role: res.role,
+                  status: res.status,
+                })
+                .then((r) => {
+                  let pageSize = r.data.data.totalRecord;
+                  let dataSource = this.getDataSourceByDataList(
+                    r.data.data.list
+                  );
+                  this.setState({
+                    dataSource: dataSource,
+                    loading: false,
+                    pageSize,
+                  });
+                })
+                .catch(() => {
+                  // TODO 把异常显示到页面中
+                  this.setState({
+                    loading: false,
+                  });
+                });
+            }}
+          >
+            <Form.Item
+              label={
+                <div style={{ fontWeight: 900, color: "rgb(127,125,142)" }}>
+                  Job ID
+                </div>
+              }
+              name="id"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label={
+                <div style={{ fontWeight: 900, color: "rgb(127,125,142)" }}>
+                  Role
+                </div>
+              }
+              name="role"
+            >
+              <Select
+                mode="multiple"
+                placeholder="Select Role"
+                style={{ width: "8vw" }}
+              >
+                <Option value={"guest"}>guest</Option>
+                <Option value={"host"}>host</Option>
+                <Option value={"arbiter"}>arbiter</Option>
+                <Option value={"local"}>local</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label={
+                <div style={{ fontWeight: 900, color: "rgb(127,125,142)" }}>
+                  Party ID
+                </div>
+              }
+              name="partyId"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label={
+                <div style={{ fontWeight: 900, color: "rgb(127,125,142)" }}>
+                  Status
+                </div>
+              }
+              name="status"
+            >
+              <Select
+                mode="multiple"
+                placeholder="Select Status"
+                style={{ width: "8vw" }}
+              >
+                <Option value={"success"}>success</Option>
+                <Option value={"running"}>running</Option>
+                <Option value={"waiting"}>waiting</Option>
+                <Option value={"failed"}>failed</Option>
+                <Option value={"canceled"}>canceled</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label={
+                <div style={{ fontWeight: 900, color: "rgb(127,125,142)" }}>
+                  note
+                </div>
+              }
+              name="note"
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                style={{ borderRadius: "5vw", width: "4vw" }}
+                type="primary"
+                htmlType="submit"
+              >
+                搜索
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
         <Table
           loading={this.state.loading}
-          scroll={{ y: "64vh" }}
+          scroll={{ y: "62vh" }}
           bordered={false}
           dataSource={this.state.dataSource}
           columns={this.state.columns}
-          pagination={{ pageSize: 25 }}
+          pagination={{
+            pageSize: 20,
+            position: ["bottomCenter"],
+            size: "small",
+            total: this.state.pageSize,
+            onChange: (page, pageSize) => {
+              this.setState({ loading: true });
+              axios
+                .post(api.pageList, {
+                  fDescription: "",
+                  jobId: "",
+                  job_id: "",
+                  note: "",
+                  orderField: "f_job_id",
+                  orderRule: "desc",
+                  pageNum: page,
+                  pageSize: 20,
+                  partyId: "",
+                  party_id: "",
+                  role: [],
+                  status: [],
+                })
+                .then((r) => {
+                  let list = r.data.data.list;
+                  let pageSize = r.data.data.totalRecord;
+                  let dataSource = this.getDataSourceByDataList(list);
+                  this.setState({
+                    dataSource,
+                    loading: false,
+                    pageSize,
+                  });
+                })
+                .catch(() => {
+                  // TODO 把异常显示到页面中
+                  this.setState({
+                    loading: false,
+                  });
+                });
+            },
+          }}
         />
       </div>
     );
