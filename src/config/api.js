2793abf83ce15ca498ccff1bd3f998948e214b2d
iosyyy <626797813@qq.com>
2021-05-05 21:39:53 +0800

    #fix 增加api

--- src/config/api.js
+++ src/config/api.js
@@ -7,7 +7,7 @@ const e = {
   federalTrain: host + api1 + "/fed/service/",
   host,
   serverSocketIp: "http://localhost:8080",
-  pageList: "http://127.0.0.1:8080/job/query/page/new",
+  pageList: "http://127.0.0.1:8080/job/query/page/new", // 详情界面url
   showList: "ws://127.0.0.1:8080/websocket/progress/{jobId}/{role}/{partyId}", // TODO 中间的Show组件的url这里使用websocket链接注意一下
   showDetailMetrics: "http://127.0.0.1:8080/v1/tracking/component/metrics",
   showDetailOutputModel:
