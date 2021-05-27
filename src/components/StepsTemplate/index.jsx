e9ebd65bcb71192f0fb95e9d25d1b747c1c9de4a
iosyyy <626797813@qq.com>
2021-05-27 18:12:06 +0800

    #fix 增加样式

---
+++ src/components/StepsTemplate/index.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import {
+  CloudUploadOutlined,
+  DownloadOutlined,
+  FileOutlined,
+} from "@ant-design/icons";
+import { Steps } from "antd";
+
+const { Step } = Steps;
+class StepsTemplate extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { steps } = this.props;
+    return (
+      <Steps style={{ marginBottom: "1  vh" }}>
+        {steps.map((v, i) => {
+          return (
+            <Step key={i} status={v.status} title={v.title} icon={v.icon} />
+          );
+        })}
+      </Steps>
+    );
+  }
+}
+
+export default StepsTemplate;
