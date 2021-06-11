e0189f8b16aaccf56ea9169d6089a31804a6adca
Aurora-NB <772335766@qq.com>
2021-06-11 21:11:10 +0800

    feat:加个表单

--- src/pages/DataSource/DatasourceHandle/datasourceRuleTable.jsx
+++ src/pages/DataSource/DatasourceHandle/datasourceRuleTable.jsx
@@ -1,9 +1,118 @@
 import React, { Component } from "react";
-import { Table } from "antd";
+import { Table, Select, Button, Space } from "antd";
+const { Option } = Select;
 
+const dataSource = [
+  {
+    key: "1",
+    fieldLabel: "...",
+    fieldName: "...",
+    map: "",
+  },
+  {
+    key: "2",
+    fieldLabel: "...",
+    fieldName: "...",
+    map: "",
+  },
+  {
+    key: "3",
+    fieldLabel: "...",
+    fieldName: "...",
+    map: "",
+  },
+  {
+    key: "4",
+    fieldLabel: "...",
+    fieldName: "...",
+    map: "",
+  },
+  {
+    key: "5",
+    fieldLabel: "...",
+    fieldName: "...",
+    map: "",
+  },
+];
+
+const COLUMNS = [
+  {
+    title: "",
+    dataIndex: "index",
+    key: "index",
+    render: (text, record, index) => {
+      return <h1>{index}</h1>;
+    },
+    width: "5vw",
+  },
+  {
+    title: "字段标签",
+    dataIndex: "fieldLabel",
+    key: "fieldLabel",
+    width: "10vw",
+  },
+  {
+    title: "字段名称",
+    dataIndex: "fieldName",
+    key: "fieldName",
+    width: "10vw",
+  },
+  {
+    title: "字段类型",
+    dataIndex: "fieldType",
+    key: "fieldType",
+    render: (text, record, index) => {
+      return (
+        <Select defaultValue='id'>
+          <Option value='id'>唯一标识(ID)</Option>
+          <Option value='integer'>数值型变量</Option>
+          <Option value='type'>分类型变量</Option>
+        </Select>
+      );
+    },
+    width: "10vw",
+  },
+  {
+    title: "是否使用",
+    dataIndex: "use",
+    key: "use",
+    render: (text, record, index) => {
+      return (
+        <Select defaultValue='yes'>
+          <Option value='yes'>是</Option>
+          <Option value='no'>否</Option>
+        </Select>
+      );
+    },
+    width: "10vw",
+  },
+  {
+    title: "标签(分类)Map (哑变量处理)",
+    dataIndex: "map",
+    key: "map",
+    width: "10vw",
+  },
+];
 class DatasourceRuleTable extends Component {
   render() {
-    return <Table />;
+    return (
+      <div>
+        <Table
+          dataSource={dataSource}
+          columns={COLUMNS}
+          size='small'
+          pagination={false}
+        ></Table>
+        <Space style={{ marginTop: "3vh" }} size='large'>
+          <Button size='large' type='primary'>
+            保存
+          </Button>
+          <Button size='large' type='primary'>
+            关闭
+          </Button>
+        </Space>
+      </div>
+    );
   }
 }
 
--- src/pages/DataSource/DatasourceHandle/index.jsx
+++ src/pages/DataSource/DatasourceHandle/index.jsx
@@ -7,15 +7,15 @@ class DatasourceHandle extends Component {
   render() {
     return (
       <div
-        style={{ height: "83vh", width: "83vw" }}
-        className="site-layout-content"
+        style={{ height: "83vh", width: "83vw", overflow: "scroll" }}
+        className='site-layout-content'
       >
-        <h2 className="colorWhite">数据源预处理</h2>
-        <Divider orientation="left">
+        <h2 className='colorWhite'>数据源预处理</h2>
+        <Divider orientation='left'>
           <h3 style={{ color: "rgb(65,89,209)" }}>基本信息</h3>
         </Divider>
         <DatasourceFormHandle />
-        <Divider orientation="left">
+        <Divider orientation='left'>
           <h3 style={{ color: "rgb(65,89,209)" }}>数据预处理字段规则定义</h3>
         </Divider>
         <DatasourceRuleTable />
