0ad7edc483c4c9b16eee3d8f772b8664aca71378
iosyyy <626797813@qq.com>
2021-08-01 14:52:07 +0800

    #fix 修复部分bug

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,10 @@ class App extends Component {
     axios
       .get(api.getClientInfo)
       .then((r) => {
-        if (r.data.data.code) {
+        if (r.data.data.retcode || r.data.data.code) {
           message.error("服务验证失败,请重试");
         } else {
-          const { party_name, party_id } = r.data.data.data;
+          const { party_name, party_id } = r.data.data.data || r.data.data;
           this.setState({
             party_name,
             party_id,
